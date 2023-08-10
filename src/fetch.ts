ba61a4a8b2a06ee3c2497d7a4511e63a1e2caeae
Rui Costa <ruicosta042@gmail.com>
2023-08-10 01:15:19 +0100

    chore: update deps, security fixes

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,27 +1,26 @@
-import { request } from "node:http";
-import type { RequestOptions } from "node:http";
+import { request, type RequestOptions } from "node:http";
 import { parse } from "node:url";
 
 export async function fetch(url: string, options: RequestOptions) {
-  const parsed = parse(url);
-  const opts: RequestOptions = {
-    method: "GET",
-    ...parsed,
-    ...options,
-  };
+	const parsed = parse(url);
+	const opts: RequestOptions = {
+		method: "GET",
+		...parsed,
+		...options,
+	};
 
-  return new Promise<void>((resolve, reject) => {
-    const req = request(opts, (res) => {
-      const { statusCode = 500 } = res;
-      if (statusCode < 400 || statusCode > 500) {
-        resolve();
-      } else {
-        reject();
-      }
-    });
+	return new Promise<void>((resolve, reject) => {
+		const req = request(opts, (res) => {
+			const { statusCode = 500 } = res;
+			if (statusCode < 400 || statusCode > 500) {
+				resolve();
+			} else {
+				reject();
+			}
+		});
 
-    req.on("error", reject);
+		req.on("error", reject);
 
-    req.end();
-  });
+		req.end();
+	});
 }
