61a6947304be043e71cc10e1baf39a347a0f4223
Rui Costa <ruicosta042@gmail.com>
2023-10-27 02:21:20 +0100

    fix: drop sleep

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ export { fetch } from "./fetch";
 export { getLocalIp } from "./get-local-ip";
 export { getRandomPort } from "./get-random-port";
 export { $ } from "./shell";
-export { sleep } from "./sleep";
 export { waitForFile } from "./wait-for-file";
 export { waitForFileSize } from "./wait-for-file-size";
 export { waitForSocket } from "./wait-for-socket";
