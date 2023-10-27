15a767e2c797a0124058b798ed2d61a2fe515f0c
Rui Costa <ruicosta042@gmail.com>
2023-10-27 03:10:58 +0100

    fix: drop getRandomPort

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export { fetch } from "./fetch";
 export { getLocalIp } from "./get-local-ip";
-export { getRandomPort } from "./get-random-port";
 export { $ } from "./shell";
 export { waitForFileSize } from "./wait-for-file-size";
 export { waitForSocket } from "./wait-for-socket";
