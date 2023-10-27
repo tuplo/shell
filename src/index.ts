81637611abf0e361cf2e7b382ad215f296087a58
Rui Costa <ruicosta042@gmail.com>
2023-10-27 02:50:30 +0100

    fix: drop waitForFile

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ export { fetch } from "./fetch";
 export { getLocalIp } from "./get-local-ip";
 export { getRandomPort } from "./get-random-port";
 export { $ } from "./shell";
-export { waitForFile } from "./wait-for-file";
 export { waitForFileSize } from "./wait-for-file-size";
 export { waitForSocket } from "./wait-for-socket";
